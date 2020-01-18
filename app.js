b30ff1154f18a7eaec31ccb288dab132d59c5e4a
Wolfleader <james.morgan@iinet.net.au>
2020-01-18 13:45:23 +0800

    reworking params

--- app.js
+++ app.js
@@ -2,24 +2,23 @@ const config = require('./config');
 const twit = require('twit');
 const T = new twit(config);
 
-const params = {
-    q:'#nodejs, #Nodejs, #GameDev, #100DaysOfCode, #Programming, #c++, #C#, #Unity, #UnrealEngine, #Coding, #JS, #Javascript', 
-    result_type: 'recent',
-    lang: 'en' 
-}
-
 console.log("==============================");
 console.log("=                            =");
 console.log("=     STARTING Wolfee Bot    =");
 console.log("=                            =");
 console.log("==============================");
+const params = {
+    q:'#100DaysOfCode, #Nodejs, #GameDev, #100DaysOfCode, #Programming, #c++, #C#, #Unity, #UnrealEngine, #Coding, #JS, #Javascript',
+    count: 10,
+    result_type: 'popular',
+    lang: 'en' 
+}
+
 function retweet() {
-    console.log("TEST1111");
     searchTweets(params);
-    console.log("TEST");
     
 }
-setInterval(retweet, 5000);
+setInterval(retweet, 10000);
 
 function searchTweets(params) {
     T.get('search/tweets', params, (err, data, response) => {
@@ -27,20 +26,24 @@ function searchTweets(params) {
         if (!err) {
             for(let dat of tweets) {
                 let retweetID = dat.id_str;
-                postTweet(retweetID);
+                postTweet(retweetID);                
             }
         } else {
             console.log("ERROR");
+            console.log(err);
+            
         }
     });
 }
 function postTweet(retweetID) {
-    T.post('statuses/retweet/:id', {id: retweetID},(err, response) => {
+    T.post('statuses/retweet/:id', {id: retweetID},(err, data, response) => {
+        console.log(data);
+        
         if (response) {
             console.log("Retweeted tweet: " + retweetID);
         }
         if (err) {
-            console.log("An error occured when trying to retweet");
+            console.log("An error occured when trying to retweet (The tweet probably already exsists)");
             
         }
     });
