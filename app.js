321ae40582621718a411cd8d453100be36980adf
Wolfleader <james.morgan@iinet.net.au>
2020-01-19 10:15:17 +0800

    Refactored code
    refactored my code making it easier for people to use

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const config = require('./config');
-const retweet = require('./retweet');
+const { retweet } = require('./retweet');
 const twit = require('twit');
 const T = new twit(config);
 
-var minutes = 10;
-_TIME = Math.floor(minutes * 60000);
+// Set how often to retweet recent tweets
+const _TIME_RECENT_MINS = 10;
+
+// Set how often to retweet recent tweets
+const _TIME_POPULAR_MINS = 30;
+
 
 const chalk = require('chalk');
 console.log(chalk.green("=============================="));
@@ -13,4 +17,8 @@ console.log(chalk.green("=    "),chalk.magenta("STARTING Wolfee Bot"), chalk.gre
 console.log(chalk.green("=                            ="));
 console.log(chalk.green("=============================="));
 
-retweet(_TIME);
+        /* IGNORE EVERYTHING BELOW THIS LINE */
+
+const _TIME_RECENT = Math.floor(_TIME_RECENT_MINS * 60000);
+const _TIME_POPULAR = Math.floor(_TIME_POPULAR_MINS * 60000);
+retweet(_TIME_RECENT, _TIME_POPULAR);
--- retweet.js
+++ retweet.js
@@ -4,53 +4,108 @@ const T = new twit(config);
 
 const chalk = require('chalk');
 
-module.exports = (_TIME) => {
-    console.log(chalk.red.bold.underline("\nStarting up Retweet Script..."));
-    console.log(chalk.blue.bgBlue("================================================================================================="));
-    console.log(chalk.yellow.bold("Retweets are set to retweet every 10 Minutes. This can be changed in the _TIME variable in app.js"));
-    console.log(chalk.blue.bgBlue("================================================================================================="))
-    
-    
-    // START OF SCRIPT
-        const params = {
-            q:'#100DaysOfCode OR #Nodejs OR #GameDev OR #100DaysOfCode OR #Programming OR #c++ OR #C# OR #Unity OR #UnrealEngine OR #Coding OR #JS OR #Javascript OR #WebDev',
-            count: 10,
-            result_type: 'recent',
-            lang: 'en' 
-        }
-        
-        function retweet() {
-            searchTweets(params);
-            
-        }
-        setInterval(retweet, _TIME);
-        
-        function searchTweets(params) {
-            T.get('search/tweets', params, (err, data, response) => {
-                let tweets = data.statuses;
-                if (!err) {
-                    for(let dat of tweets) {
-                        let retweetID = dat.id_str;
-                        postTweet(retweetID);                
-                    }
-                } else {
-                    console.log("ERROR");
-                    console.log(err);
-                    
-                }
-            });
-        }
-        function postTweet(retweetID) {
-            T.post('statuses/retweet/:id', {id: retweetID},(err, data, response) => {
-                console.log(data);
-                if (response) {
-                    console.log("Retweeted tweet: " + retweetID);
-                }
-                if (err) {
-                    console.log("An error occured when trying to retweet (The tweet probably already exsists)");
-                    
-                }
-            });
-        }
-}
+var recent = (_TIME_RECENT) => {
 
+    // START OF RECENT Retweets
+    const params = {
+        q: '#100DaysOfCode OR #Nodejs OR #GameDev OR #100DaysOfCode OR #Programming OR #c++ OR #C# OR #Unity OR #UnrealEngine OR #Coding OR #JS OR #Javascript OR #WebDev',
+        count: 15,
+        result_type: 'recent',
+        lang: 'en'
+    }
+
+    function retweet() {
+        searchTweets(params);
+
+    }
+    setInterval(retweet, _TIME_RECENT);
+
+    function searchTweets(params) {
+        T.get('search/tweets', params, (err, data, response) => {
+            let tweets = data.statuses;
+            if (!err) {
+                for (let dat of tweets) {
+                    let retweetID = dat.id_str;
+                    postTweet(retweetID);
+                }
+            } else {
+                console.log("ERROR");
+                console.log(err);
+
+            }
+        });
+    }
+
+    function postTweet(retweetID) {
+        T.post('statuses/retweet/:id', {
+            id: retweetID
+        }, (err, data, response) => {
+            console.log(data);
+            if (response) {
+                console.log("Retweeted tweet: " + retweetID);
+            }
+            if (err) {
+                console.log("An error occured when trying to retweet (The tweet probably already exsists)");
+
+            }
+        });
+    }
+};
+var popular = (_TIME_POPULAR) => {
+
+    // START OF POPULAR retweets
+    const params = {
+        q: '#100DaysOfCode OR #Nodejs OR #GameDev OR #100DaysOfCode OR #Programming OR #c++ OR #C# OR #Unity OR #UnrealEngine OR #Coding OR #JS OR #Javascript OR #WebDev',
+        count: 100,
+        result_type: 'popular',
+        lang: 'en'
+    }
+
+    function retweet() {
+        searchTweets(params);
+
+    }
+    setInterval(retweet, _TIME_POPULAR);
+
+    function searchTweets(params) {
+        T.get('search/tweets', params, (err, data, response) => {
+            let tweets = data.statuses;
+            if (!err) {
+                for (let dat of tweets) {
+                    let retweetID = dat.id_str;
+                    postTweet(retweetID);
+                }
+            } else {
+                console.log("ERROR");
+                console.log(err);
+
+            }
+        });
+    }
+
+    function postTweet(retweetID) {
+        T.post('statuses/retweet/:id', {
+            id: retweetID
+        }, (err, data, response) => {
+            console.log(data);
+            if (response) {
+                console.log("Retweeted tweet: " + retweetID);
+            }
+            if (err) {
+                console.log("An error occured when trying to retweet (The tweet probably already exsists)");
+
+            }
+        });
+    }
+};
+
+module.exports = {
+    retweet: (_TIME_RECENT, _TIME_POPULAR) => {
+        console.log(chalk.red.bold.underline("\nStarting up Retweet Script..."));
+        console.log(chalk.blue.bgBlue("================================================================================================="));
+        console.log(chalk.yellow.bold("Retweets are set to retweet popular tweets every 30 Minutes and recent tweets every 10 Minutes.\n  This can be changed in the _TIME variables in app.js"));
+        console.log(chalk.blue.bgBlue("================================================================================================="));
+        recent(_TIME_RECENT);
+        popular(_TIME_POPULAR);
+    }
+}
